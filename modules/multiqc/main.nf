2ed139a23d74779bafaccba7bf7c8b3f2dd2508e
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2024-06-25 10:04:40 +0000

    update pkg versions for conda
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- modules/multiqc/main.nf
+++ modules/multiqc/main.nf
@@ -1,7 +1,7 @@
 params.outdir = 'results'
 
 process MULTIQC {
-    conda 'bioconda::multiqc=1.17'
+    conda 'bioconda::multiqc=1.22.1'
     publishDir params.outdir, mode:'copy'
 
     input:
--- wave-cli-example.sh
+++ wave-cli-example.sh
@@ -3,9 +3,9 @@
 # get container URLs for SIF images
 nextflow inspect . -profile waveSing
 
-wave --conda-package bioconda::multiqc=1.17 --singularity --freeze
+wave --conda-package bioconda::multiqc=1.22.1 --singularity --freeze
 
 # get container URLs for Docker images
 nextflow inspect . -profile waveSingPull
 
-wave --conda-package bioconda::multiqc=1.17 --freeze
+wave --conda-package bioconda::multiqc=1.22.1 --freeze
